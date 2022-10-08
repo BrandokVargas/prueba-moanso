f17bf75420c82ce40166d6d1e1ce3f916ab850f6
BrandokVargas <brandokvargas27@gmail.com>
2022-10-08 15:12:52 -0500

    Primer commit prueba moanso

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace CONVERTIDOR_ESCALA_0_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRed = new System.Windows.Forms.TextBox();
+            this.txtGreen = new System.Windows.Forms.TextBox();
+            this.txtBlue = new System.Windows.Forms.TextBox();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.lblGreen = new System.Windows.Forms.Label();
+            this.lblBlue = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(117, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(558, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONVERTIDOR ESCALA PARA COMPUTACIÓN GRAFICA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RED";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "GREEN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(83, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "BLUE";
+            // 
+            // txtRed
+            // 
+            this.txtRed.Location = new System.Drawing.Point(198, 154);
+            this.txtRed.Name = "txtRed";
+            this.txtRed.Size = new System.Drawing.Size(116, 20);
+            this.txtRed.TabIndex = 4;
+            // 
+            // txtGreen
+            // 
+            this.txtGreen.Location = new System.Drawing.Point(198, 207);
+            this.txtGreen.Name = "txtGreen";
+            this.txtGreen.Size = new System.Drawing.Size(116, 20);
+            this.txtGreen.TabIndex = 4;
+            // 
+            // txtBlue
+            // 
+            this.txtBlue.Location = new System.Drawing.Point(198, 273);
+            this.txtBlue.Name = "txtBlue";
+            this.txtBlue.Size = new System.Drawing.Size(116, 20);
+            this.txtBlue.TabIndex = 4;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.Location = new System.Drawing.Point(364, 182);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(129, 66);
+            this.btnConvertir.TabIndex = 5;
+            this.btnConvertir.Text = "CONVERTIR";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(555, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 29);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "RED";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(555, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 29);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "GREEN";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(555, 264);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 29);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "BLUE";
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRed.Location = new System.Drawing.Point(674, 145);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(20, 29);
+            this.lblRed.TabIndex = 1;
+            this.lblRed.Text = "-";
+            // 
+            // lblGreen
+            // 
+            this.lblGreen.AutoSize = true;
+            this.lblGreen.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGreen.Location = new System.Drawing.Point(674, 207);
+            this.lblGreen.Name = "lblGreen";
+            this.lblGreen.Size = new System.Drawing.Size(20, 29);
+            this.lblGreen.TabIndex = 1;
+            this.lblGreen.Text = "-";
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.AutoSize = true;
+            this.lblBlue.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBlue.Location = new System.Drawing.Point(674, 264);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(20, 29);
+            this.lblBlue.TabIndex = 1;
+            this.lblBlue.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.txtBlue);
+            this.Controls.Add(this.txtGreen);
+            this.Controls.Add(this.txtRed);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblBlue);
+            this.Controls.Add(this.lblGreen);
+            this.Controls.Add(this.lblRed);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CONVERTIDOR BY BRANDOK VARGAS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRed;
+        private System.Windows.Forms.TextBox txtGreen;
+        private System.Windows.Forms.TextBox txtBlue;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label lblGreen;
+        private System.Windows.Forms.Label lblBlue;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CONVERTIDOR_ESCALA_0_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+
+            double red = Convert.ToInt32(this.txtRed.Text);
+            double green = Convert.ToInt32(this.txtGreen.Text);
+            double blue = Convert.ToInt32(this.txtBlue.Text);
+
+            double r,g,b;
+
+
+
+            //Convert
+            const int porcentaje = 100;
+            const int ultimoValorRgb = 255;
+            r = ((red * porcentaje) / ultimoValorRgb) / porcentaje;
+            g = ((green * porcentaje) / ultimoValorRgb) / porcentaje;
+            b = ((blue * porcentaje) / ultimoValorRgb) / porcentaje;
+            
+            this.lblRed.Text = r.ToString("N4");
+            this.lblGreen.Text = g.ToString("N4");
+            this.lblBlue.Text = b.ToString("N4");
+
+
+        }
+    }
+}
